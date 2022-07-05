1a3a74a3b706f104ca70e5830f5ac37f565b1e34
Yash Mehta <yashmehta2410@gmail.com>
2022-07-04 23:39:31 -0400

    Foodiffin commit

---
+++ app/src/main/java/com/example/foodiffin/activities/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.foodiffin.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.foodiffin.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    TextView logInText;
+    TextView signUpText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        logInText=findViewById(R.id.logInTextView);
+        signUpText=findViewById(R.id.signUpTextView);
+        logInText.setOnClickListener(view -> startActivity(new Intent(WelcomeActivity.this,LogInActivity.class)));
+
+        signUpText.setOnClickListener(view -> startActivity(new Intent(WelcomeActivity.this,SignUpActivity.class)));
+    }
+}
